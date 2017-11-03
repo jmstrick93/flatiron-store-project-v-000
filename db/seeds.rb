472660bd9fb515724fcca3c12d459b2abfcaa4d6
Jim Stricker <jmstricker93@gmail.com>
2017-11-03 16:11:58 -0400

    make CreateCarts migration update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-10.times do 
+#seed_items
+
+10.times do
   Item.create(
-    title: Faker::Commerce.product_name, 
-    inventory: Faker::Number.number(2), 
+    title: Faker::Commerce.product_name,
+    inventory: Faker::Number.number(2),
     price: Faker::Number.number(4)
   )
   Category.create(title: Faker::Commerce.department)
@@ -12,4 +14,17 @@ Item.all.each do |item|
   item.category_id = counter
   item.save
   counter += 1
+end
+
+#seed_users
+
+10.times do
+  name = Faker::Name.name
+  email = Faker::Internet.email(name)
+  password = Faker::Internet.password
+  User.create(
+    name: name
+    email: email
+    password: password
+  )
 end
