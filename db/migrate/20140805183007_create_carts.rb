11913403d68bd238e11a1025d086fd066b9a5b0e
Jim Stricker <jmstricker93@gmail.com>
2017-11-04 12:47:27 -0400

    pre migration commit

--- db/migrate/20140805183007_create_carts.rb
+++ db/migrate/20140805183007_create_carts.rb
@@ -2,7 +2,7 @@ class CreateCarts < ActiveRecord::Migration
   def change
     create_table :cart do |t|
       t.integer :user_id
-      t.integer :item_id
+      t.timestamps :null => false
     end
   end
 end
--- db/migrate/20140805200233_create_orders.rb
+++ db/migrate/20140805200233_create_orders.rb
@@ -1,5 +1,8 @@
 class CreateOrders < ActiveRecord::Migration
   def change
-
+    create_table :cart do |t|
+      t.integer :user_id
+      t.timestamps :null => false
+    end
   end
 end
