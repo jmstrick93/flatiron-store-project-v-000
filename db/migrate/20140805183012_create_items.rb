1bc09f0be4eb8d8745253e99e5f1ce727b82bdd6
Jim Stricker <jmstricker93@gmail.com>
2017-11-04 12:49:16 -0400

    modified item.price scale/precision after initial migration failure

--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
-      t.decimal :price, scale: 2
+      t.decimal :price, precision: 5, scale: 2
       t.integer :category_id
       t.timestamps :null => false
     end
