0df502f25f1af8f3b5a70a9494ef7b5a3eda09a5
Jim Stricker <jmstricker93@gmail.com>
2017-11-04 12:51:10 -0400

    fixed migration issues -- tables now (hopefully) working as intended

--- db/migrate/20171104160135_order_items.rb
+++ db/migrate/20171104160135_order_items.rb
@@ -1,6 +1,6 @@
 class OrderItems < ActiveRecord::Migration
   def change
-    create_table :line_items do |t|
+    create_table :order_items do |t|
       t.integer :order_id
       t.integer :item_id
       t.integer :quantity, default: 1
