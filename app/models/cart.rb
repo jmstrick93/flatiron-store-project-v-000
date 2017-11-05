0300da7db19a9ac05c167511704b1406f38b1db7
Jim Stricker <jmstricker93@gmail.com>
2017-11-05 16:13:08 -0500

    Cart model now fully functioning

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,30 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
   has_many :items, through: :line_items
+
+
+  def total
+    sum = 0
+    items.each do |item|
+      sum += item.price
+    end
+    sum
+  end
+
+  def add_item(item_id)
+    line_item = LineItem.new(cart_id: self.id, item_id: item_id, quantity: 1)
+
+    if line_item.already_in_cart?
+      #dont create a new line item, but rather just increase the quantity of the old one
+      old_line_item = LineItem.find_by(item_id: item_id, cart_id: id)
+      old_line_item.quantity += line_item.quantity
+      old_line_item.save
+      line_item = old_line_item
+    end
+    #return the item that was effected
+    line_item
+  end
+
+
+
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,13 @@
 class LineItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :item
+
+
+
+
+  def already_in_cart?
+    #use on unpersisted line items to determine if one is already in the cart that you are trying to put the receiving one in.
+    self.class.find_by(cart_id: cart_id, item_id: item_id) ? true : false
+  end
+
 end
