b61acc1242f25b56d4d9b5a3c09dfce7f0ccb704
Jim Stricker <jmstricker93@gmail.com>
2017-11-06 11:16:35 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,17 @@ class SessionsController < ApplicationController
   def create
     if @user = User.find_by(email: params[:email])
       if @user.authenticate(params[:password])
+        #result if login credentials are correct
         session[:user_id] = @user.id
         flash[:notice] = "Successfully Logged In"
         redirect_to store_path
       else
+        #result if email correct, password incorrect
         flash[:alert] = "Invalid Password"
         redirect_to login_path
       end
-
     else
+      #result if email address correct.  Password has not been checked
       flash[:alert] = "Invalid Email Address"
       redirect_to login_path
     end
