07f99e9f381b42800964e6131d8dd10d20f0c31d
Jim Stricker <jmstricker93@gmail.com>
2017-11-06 19:04:31 -0500

    created User#return_or_set_current_cart and current_cart button, and signout capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,30 @@ class User < ActiveRecord::Base
   has_many :orders
   #eventually find a way to have many items through carts (i know there's a way but not straightforward, maybe Delegate?)
 
+
+
   def current_cart=(cart)
     cart.save if !cart.persisted?
     self.current_cart_id = cart.id
+    self.save
   end
 
   def current_cart
     Cart.all.find_by(:id => self.current_cart_id)
+  end
+
+  def return_or_set_current_cart
+    if !self.current_cart
+
+      current_cart = self.carts.create
+      self.current_cart_id = current_cart.id
+      self.save
+
+      current_cart
+    else
+
+      self.current_cart
+    end
   end
 
 
