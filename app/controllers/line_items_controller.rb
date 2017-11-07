486db3e8f9669117d27eb49ea9d4f2bb80d9b85b
Jim Stricker <jmstricker93@gmail.com>
2017-11-07 09:54:51 -0500

    all tests passing!

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   def create
     @user = current_user
+    @user.current_cart = @user.carts.build if !@user.current_cart
     @cart = @user.current_cart
     @item = Item.find_by(:id => params[:item_id])
     @cart.add_item(@item.id).save
@@ -8,4 +9,3 @@ class LineItemsController < ApplicationController
     redirect_to cart_path(@cart)
   end
 end
-  
